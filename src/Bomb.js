40ec99fd7b8890da51f823f30fd43b48ef140568
PhilLeeChin <phil.leechin@outlook.com>
2021-06-28 22:57:18 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+
+        this.state = {
+            secondsRemaining: props.initialCount
+        }
+    }
+
+    render() {
+        const message = this.state.secondsRemaining === 0 ? 'Boom!' : `${this.state.secondsRemaining} seconds remaining before explosion`;
+
+        return (
+            <div>{message}</div>
+        )
+    }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
 // your ImageSlider code here!
+import React from 'react' ;
+
+class ImageSlider extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            currentIndex: 0
+        }
+    }
+    render() {
+        return (
+            <h1>I am on slide {this.state.currentIndex}</h1>
+        )
+    }
+}
+
+export default ImageSlider;
